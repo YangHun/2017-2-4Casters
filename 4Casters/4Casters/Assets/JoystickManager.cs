6ff36c227b03c1ff7ad8839594a985a9cd2237ed
16김민성 <foat3376@korea.ac.kr>
2018-01-20 18:32:53 +0900

    Add Joystick on New Scene

---
+++ 4Casters/4Casters/Assets/JoystickManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class JoystickManager : MonoBehaviour {
+
+	[SerializeField]
+	int stickSize = 40;
+	string selectName;
+
+    public void OnPointerDown(Selectable s)
+    {
+        selectName = s.name;
+        if(selectName == "JoystickBody")
+        {
+            Debug.Log("Body was pressed");
+			RectTransform stick = (RectTransform)s.transform.GetChild(0);
+            stick.sizeDelta = new Vector2(stickSize, stickSize);
+        }
+        return;
+		
+    }
+
+    public void OnPointerUp(Selectable s)
+    {
+        selectName = s.name;
+        if (selectName == "JoystickBody")
+        {
+            Debug.Log("Body was released");
+            RectTransform c = (RectTransform)s.transform.GetChild(0);
+            c.sizeDelta = new Vector2(0, 0);
+            c.position = new Vector3(0, 0, 0);
+        }
+        return;
+    }
+
+	void Start ()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
